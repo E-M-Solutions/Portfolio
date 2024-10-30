f473311b131bf2bcad237ce22ea923b7c1ba8f85
maxime <mxberiot@gmail.com>
2024-10-30 18:45:00 +0100

    Portfolio - V1

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-//     var sidenav = document.getElementById("mySidenav");
-//     var openBtn = document.getElementById("openBtn");
-//     var closeBtn = document.getElementById("closeBtn");
-
-
-//     console.log(sidenav);
-//     console.log(openBtn);
-//     console.log(closeBtn);
-
-//     openBtn.addEventListener("mouseenter", openNav);
-//     openBtn.addEventListener("mouseleave", closeNav);
-
-
-//     /* Set the width of the side navigation to 250px */
-// function openNav() {
-//     sidenav.classList.add("active");
-    
-//   }
-  
-//   /* Set the width of the side navigation to 0 */
-//   function closeNav() {
-//     sidenav.classList.remove("active");
-//   }
-  
-
-// // JavaScript to add active class to clicked menu item
-// document.querySelectorAll('nav ul li a').forEach(link => {
-//   link.addEventListener('click', function() {
-//       document.querySelector('nav ul li a.active').classList.remove('active');
-//       this.classList.add('active');
-//   });
-// });
-
 // Fonction de défilement fluide avec animation de l'apparition des sections
 document.addEventListener("DOMContentLoaded", () => {
   const sections = document.querySelectorAll(".content");
@@ -136,3 +103,9 @@ window.addEventListener("resize", () => {
 // Initialiser et animer les particules
 initParticles();
 animateParticles();
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    initParticles();
+});
